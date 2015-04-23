44fd1fc243ce0c2e8170b8d4e748264be626c081
Tiest Vilee <xp3@tiestvilee.fastmail.com.au>
2015-04-23 17:35:32 +0100

    seems to work, added some more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,12 @@
 ## Provides a mechanism to cache the inverse of a matrix - a
-## potentialy expensive operation.
+## potentialy expensive operation that we only want to perform
+## once if possible.
+
 
 ## makeCacheMatrix
 ## Returns a 'matrix' object that can be used to safely cache the
 ## inverse of that matrix.
+## x -> a matrix to wrap - defaults to an empty matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -22,6 +25,9 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve
 ## Given a 'matrix' as returned by makeCacheMatrix, this function
 ## will return the inverse of that 'matrix'.
+## x   -> the 'matrix' returned by makeCacheMatrix
+## ... -> addtional arguments to the 'solve' function that caculates
+##        the inverse
 
 cacheSolve <- function(x, ...) {
 	m <- x$getInverse()
